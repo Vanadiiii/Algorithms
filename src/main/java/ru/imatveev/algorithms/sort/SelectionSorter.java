49b9dcb6e8d23e3417128b8fda2cd8b5b91b200c
IvanMatveev <vanadiiii42@gmail.com>
2021-01-06 14:51:59 +0400

    fix SelectionSorter

--- src/main/java/ru/imatveev/algorithms/sort/SelectionSorter.java
+++ src/main/java/ru/imatveev/algorithms/sort/SelectionSorter.java
@@ -1,20 +1,26 @@
 package ru.imatveev.algorithms.sort;
 
+import static ru.imatveev.algorithms.sort.util.SortUtil.swap;
+
 public class SelectionSorter implements ISorter {
     @Override
     public int[] sort(int[] data) {
-        for (int i = 0; i < data.length - 1; i++) {
-            int mid = data[i];
-            int k = i;
-            for (int j = i + 1; j < data.length; j++) {
-                if (data[j] < mid) {
-                    k = j;
-                    mid = data[j];
-                }
-            }
-            data[k] = data[i];
-            data[i] = mid;
+        for (int i = 0; i < data.length; i++) {
+            int minIndex = findMinIndex(data, i, data.length - 1);
+            swap(data, i, minIndex);
         }
         return data;
+    }
+
+    private int findMinIndex(int[] data, int begin, int end) {
+        int minVal = data[begin];
+        int minIndex = begin;
+        for (int i = begin + 1; i <= end; i++) {
+            if (data[i] < minVal) {
+                minVal = data[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
     }
 }
